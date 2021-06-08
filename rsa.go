4b05650000a1020d77d20695b740f0454e5b2f54
nobishino <nobishii11@gmail.com>
2021-06-08 21:09:29 +0900

    Implement Erathosthenes

--- rsa.go
+++ rsa.go
@@ -8,3 +8,18 @@ func IsPrime(x uint64) bool {
 	}
 	return true
 }
+
+func Erathosthenes(max uint64) []uint64 {
+	isNotPrime := make([]bool, max+1)
+	var result []uint64
+	for i := uint64(2); i <= max; i++ {
+		if isNotPrime[i] {
+			continue
+		}
+		result = append(result, i)
+		for j := 2 * i; j <= max; j += i {
+			isNotPrime[j] = true
+		}
+	}
+	return result
+}
--- rsa_test.go
+++ rsa_test.go
@@ -3,6 +3,7 @@ package ncryp_test
 import (
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/nobishino/ncryp"
 )
 
@@ -38,6 +39,38 @@ func TestIsPrime(t *testing.T) {
 			got := ncryp.IsPrime(tt.in)
 			if got != tt.expect {
 				t.Errorf("expect %v, but got %v", tt.expect, got)
+			}
+		})
+	}
+}
+func TestErathosthenes(t *testing.T) {
+	testcases := [...]struct {
+		title  string
+		max    uint64
+		expect []uint64
+	}{
+		{
+			max:    2,
+			expect: []uint64{2},
+		},
+		{
+			max:    11,
+			expect: []uint64{2, 3, 5, 7, 11},
+		},
+		{
+			max: 100,
+			expect: []uint64{
+				2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+				31, 37, 41, 43, 47, 53, 59, 61, 67,
+				71, 73, 79, 83, 89, 97,
+			},
+		},
+	}
+	for _, tt := range testcases {
+		t.Run(tt.title, func(t *testing.T) {
+			got := ncryp.Erathosthenes(tt.max)
+			if diff := cmp.Diff(got, tt.expect); diff != "" {
+				t.Errorf("expect %v, but got %v. diff:\n%s", tt.expect, got, diff)
 			}
 		})
 	}
